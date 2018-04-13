8d37888c7721041edefcd2268c78ff11f3962dcc
AlexLTM <alopeztello@yahoo.es>
2018-04-13 11:17:13 +0200

    Add files via upload

--- P2-AboutMe/views/configuracion.php
+++ P2-AboutMe/views/configuracion.php
@@ -4,10 +4,8 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
-	<link href="../css/general.css" type="text/css" rel="stylesheet" />
-	<link href="../css/configuracion.css" type="text/css" rel="stylesheet" />
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
 	<title>Configuración</title>
 </head>
 
